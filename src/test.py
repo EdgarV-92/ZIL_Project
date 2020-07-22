c375ba49dc791e8e956391dc0437df25fcd19ef0
EdgarV-92 <edgar.vardanyan18@gmail.com>
2020-07-22 21:32:00 +0400

    Updateing some files

---
+++ src/test.py
@@ -0,0 +1,7 @@
+
+
+def test_model(model,data):
+
+
+if __name__ == "__main__":
+    test_model(model,test_data,wights_path)
--- src/train.py
+++ src/train.py
@@ -1,5 +1,78 @@
 from models import get_vgg16
-
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import pandas as pd
+import  tensorflow as tf
 
 def train_model(model, data, data_generator):
-    pass
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--square", type=int, default=8,
+                        help="display the square of a given number")
+    parser.add_argument("-v", "--verbosity", action="count",
+                        help="increase output verbosity")
+    args = parser.parse_args()
+    answer = args.square ** 2
+
+    print(model.summary())
+
+
+if __name__ == "__main__":
+    train_data = pd.read_csv("../data/images/animal_train_data.csv")
+    val_data = pd.read_csv("../data/images/animal_val_data.csv")
+    model = get_vgg16()
+    train_model(model,train_data,val_data)
+
+    train_datagen = ImageDataGenerator(
+        rotation_range=15,
+        brightness_range=[0.6, 1.0],
+        rescale=1. / 255,
+        shear_range=0.1,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        width_shift_range=0.1,
+        height_shift_range=0.1
+    )
+    train_generator = train_datagen.flow_from_dataframe(
+        train_data,
+        x_col='filename',
+        y_col='category_names',
+        target_size=(224, 224),
+        class_mode='categorical',
+        batch_size=1
+    )
+
+    val_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+    )
+
+    val_generator = val_datagen.flow_from_dataframe(
+        val_data,
+        x_col='filename',
+        y_col='category_names',
+        target_size=(224, 224),
+        class_mode='categorical',
+        batch_size=1
+    )
+
+    model = get_vgg16()
+    from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+    earlystop = EarlyStopping(patience=10)
+    lr_scheduler = ReduceLROnPlateau(monitor='val_acc',
+                                     patience=2,
+                                     verbose=1,
+                                     factor=0.1,
+                                     min_lr=0.00001)
+
+    callbacks = [earlystop, lr_scheduler]
+    model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(), metrics=['accuracy'])
+    history = model.fit_generator(
+        train_generator,
+        epochs=1,
+        validation_data=val_generator,
+        validation_steps=10,
+        steps_per_epoch=10,
+        callbacks=callbacks
+    )
+    # train_model()
